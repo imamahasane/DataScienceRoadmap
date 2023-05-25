2ded19297a9b3f2acf87906253c4f0b031348416
Md Imam Ahasan <emamahasane@gmail.com>
2023-05-26 05:08:42 +0600

    file.py

--- 1_Python/reading_writing.py
+++ 1_Python/reading_writing.py
@@ -1,5 +1,34 @@
 
-f = open("/Users/imamahasan/DataScience/1_Python/funny.txt", "w")
+# f = open("/Users/imamahasan/DataScience/1_Python/funny.txt", "w")
+# f.write(" I'm from Bangladesh")
+# f.close()
 
-f.write("This is Imam Ahasan")
+
+# f = open("/Users/imamahasan/DataScience/1_Python/funny.txt", "r")
+# print(f.read())
+# f.close()
+
+
+# f = open("/Users/imamahasan/DataScience/1_Python/funny.txt", "r")
+# for line in f:
+#     tokens = line.split(" ")
+#     print(str(tokens))
+
+# f.close()
+
+
+# f = open("/Users/imamahasan/DataScience/1_Python/funny.txt", "r")
+# for line in f:
+#     tokens = line.split(" ")
+#     print(len(tokens))
+
+# f.close() 
+
+
+f = open("/Users/imamahasan/DataScience/1_Python/funny.txt", "r")
+f_out = open("/Users/imamahasan/DataScience/1_Python/funny_wc.txt", "w")
+for line in f:
+    tokens = line.split(" ")
+    f_out.write(f"WordCount: {len(tokens)}, {line}")
 f.close()
+f_out.close()
