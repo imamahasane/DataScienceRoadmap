b3783ae4a78ca3420ae62b407b0956c2ba335795
Md Imam Ahasan <emamahasane@gmail.com>
2023-06-03 00:20:51 +0600

    numpy

---
+++ 2_NumPy/np.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a)
+print(a[0])
+print(a[1])
+ 
---
+++ 2_NumPy/np2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([[1, 2], [3, 4], [5, 6]])
+print(a)
+print(a.ndim)
+print(a.itemsize)
+print(a.dtype)
+print(a.size)
+print(a.shape)
+
+print("\n new \n")
+a = np.array([1, 2, 3, 4])
+print(a)
+print(a.ndim)
+print(a.itemsize)
+print(a.dtype)
+print(a.size)
+print(a.shape)
